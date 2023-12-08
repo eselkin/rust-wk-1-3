1889c97b8073c3688cd91ddaab5f5b1ac2ff17c5
Eli Selkin <eli.selkin@agile6.com>
2023-12-08 14:55:16 -0800

    silly hello world

---
+++ main.rs
@@ -0,0 +1,6 @@
+// write a main function that writes two lines. The first line is "Hello" and the second line is World followed by a lot of exclamations marks.
+
+fn main() {
+    println!("Hello");
+    println!("World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
